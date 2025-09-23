a3773d3598b8d3097f76da52bb41a575aed0e680
akashkroy <akashkroy@hotmail.com>
2025-09-23 18:53:40 +0530

    dynamic programming memoization code added

--- dynamic programming/1. intro/2. Intro and Memoization/fibonacci.cpp
+++ dynamic programming/1. intro/2. Intro and Memoization/fibonacci.cpp
@@ -8,9 +8,11 @@ int fib(int n, vector<int> &dp){
     if (n==0) return 0;
     if (n==1) return 1;
 
+    // this memoization logic is added to reduce the time complexity from exponential to linear
     if(dp[n]!=-1) return dp[n];
 
     return dp[n] = fib(n-1,dp) + fib(n-2,dp);
+
 
 }
 
